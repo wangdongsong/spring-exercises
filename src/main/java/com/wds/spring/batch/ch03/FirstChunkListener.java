616fc8180f86308245a0dd6650bca5d7b2d54cd5
wangdongsong <wangdongsong1229@163.com>
2016-03-01 22:26:49 +0800

    去掉lombok的引用

--- src/main/java/com/wds/spring/batch/ch03/FirstChunkListener.java
+++ src/main/java/com/wds/spring/batch/ch03/FirstChunkListener.java
@@ -1,7 +1,5 @@
 package com.wds.spring.batch.ch03;
 
-import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ChunkListener;
